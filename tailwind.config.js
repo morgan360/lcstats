f6da0100ebc3cf83216716b419a6f4caa002ea1a
Maeve Crossan <maevecrossan@gmail.com>
2025-12-17 13:05:19 +0000

    Install tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './templates/**/*.html',
+    './**/*.html',
+    './**/*.js',
+    './**/*.py',
+    './static/src/**/*.css'
+  ],
+  theme: { extend: {} },
+  plugins: [],
+}
