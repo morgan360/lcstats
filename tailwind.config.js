4703cc07a2d1158fa1d096d0d321a15f792d478f
Maeve Crossan <maevecrossan@gmail.com>
2025-12-18 16:51:33 +0000

    Update colour theme.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,29 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'off-white': '#F7F9FC',
-        'off-black': '#0F1214',
+        // Neutrals
+        "mist": "#F0EFEB",
 
-        'cinza-escuro': '#25282A',
-        'cinza-sereno': '#DDE5ED',
-        'verde-menta': '#A7E6D7',
-        'lavanda': '#685BC7',
-        'azul-naval': '#001871',
-      }
+        // Accents
+        "lime-glow": "#D8F878",
+        "rose-pop": "#E47CB8",
+        "violet-royal": "#9448B0",
+
+        // Darks
+        "indigo-deep": "#332277",
+        "twilight": "#173251ff",
+        "midnight": "#001C3D",
+      },
+
+      // Named gradients
+      backgroundImage: {
+        "gradient-01": "linear-gradient(180deg, #F0EFEB 0%, #D8F878 100%)",
+        "gradient-02": "linear-gradient(180deg, #F0EFEB 0%, #E47CB8 100%)",
+        "gradient-03": "linear-gradient(180deg, #E47CB8 0%, #9448B0 100%)",
+        "gradient-04":
+          "linear-gradient(180deg, #D8F878 0%, #E47CB8 45%, #9448B0 70%, #332277 100%)",
+        "gradient-05": "linear-gradient(180deg, #9448B0 0%, #001C3D 100%)",
+      },
     }
   },
   plugins: [],
